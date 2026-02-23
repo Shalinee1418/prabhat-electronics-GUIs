5f9c4f137dcf14717b51eba673480c3386dc0e65
shalineesharma <shalineesharma1418@gmail.com>
2026-02-23 12:22:47 +0530

    Create new purchase form Fixes #1

---
+++ header.php
@@ -0,0 +1,10 @@
+<body>
+    <header>
+        <h2>Prabhat Electronics</h2>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="#">About</a>
+            <a href="#">Sales</a>
+            <a href="service.php">Service</a>
+            <a href="#">Contact</a>
+        </nav>
